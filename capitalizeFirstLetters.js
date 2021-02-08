cb48bfc471d0a33dfcc38b336cdad587465f952d
anais-ctnt <anaiscotinet@gmail.com>
2021-02-08 15:37:47 +0100

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,13 @@
 const assert = require('assert');
 
+const capitalizeFirstLetters = (input) => {
+  const array = input.split(' ');
+  const output = array
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(' ');
+  return output;
+};
+
 // TESTS
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 assert.strictEqual(capitalizeFirstLetters.length, 1);
@@ -7,6 +15,5 @@ assert.strictEqual(
   capitalizeFirstLetters('i am learning TDD'),
   'I Am Learning TDD'
 );
-
 assert.strictEqual(capitalizeFirstLetters('hey'), 'Hey');
 assert.strictEqual(capitalizeFirstLetters(''), '');
